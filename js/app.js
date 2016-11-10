ba9426e6c7547544ef71a8e23aff5d61481fd9b3
John Dupper <john.dupper@me.com>
2016-11-09 20:26:06 -0500

    rearranged order of page, added navigation links, etc.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-var array = ['💸', '💰', '💵', '🤑'];
+var console, array = ['💸', '💰', '💵', '🤑'];
 var counter = 0;
 var elem = document.getElementById('page-header');
+var dollarInput = document.getElementById('dollar-input');
 
 function change() {
     'use strict';
@@ -12,3 +13,25 @@ function change() {
 }
 
 setInterval(change, 1000);
+
+document.getElementById('calculate').addEventListener('click', function () {
+    'use strict';
+    console.log('button');
+    document.getElementById('progress-id').removeAttribute('hidden');
+    document.getElementById('dollar-input').style.display = 'none';
+});
+
+dollarInput.addEventListener('keyup', function () {
+    'use strict';
+    console.log('key');
+    var dollarAmount, dollarSign = false;
+    dollarAmount = this.value;
+    console.log(dollarAmount);
+
+    if (dollarAmount.length >= 1) {
+        console.log('works');
+        dollarInput.value = "$" + dollarAmount;
+    } else {
+        dollarSign
+    }
+});
